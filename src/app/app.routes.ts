adf09f27cc9444ec73586048151ef87e23302a51
Ankit Kumar <ankitkumarm9546@gmail.com>
2026-01-06 22:07:06 +0530

    platform removed

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,5 @@
 import { Routes } from '@angular/router';
 import { SchoolComponent } from './component/home/school/school.component';
-import { PlatformComponent } from './component/home/platform/platform.component';
-
 export const routes: Routes = [
   { path: '', component: SchoolComponent },
-  { path: 'platform', component: PlatformComponent }
 ];
